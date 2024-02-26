8722eaa8aca18bf9a72596b67d8e647a694c9b15
MujtabaNiazi-2023 <155872344+MujtabaNiazi-2023@users.noreply.github.com>
2024-02-25 18:22:06 -0700

    css.templates

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-let userPassword = document.querySelector("#password");
-let userName = document.querySelector("#name");
-let userEmail = document.querySelector("#email-id");
-let emailInput = document.querySelector("#email");
-let inputCheck = document.querySelector("#check");
-let formBox = document.querySelector("#form-box");
-let signUp = document.querySelector("#signup");
-let formHeader = document.querySelector("#header");
+const opendiv = document.querySelector(".navbar-item");
+const boxstatus = document.querySelector(".navsidebar");
+const closediv = document.querySelector(".closemenu");
 
-userPassword.onclick = function () {
-  inputCheck.innerHTML = `<span style='color:gray'>Password should be A-Z, a-z, symbols</span>`;
-};
-userName.onclick = function () {
-  inputCheck.innerHTML = "";
-};
-signUp.onclick = function () {
-  emailInput.remove();
-  signUp.innerHTML = "Login";
-  formHeader.innerHTML = "Login Form";
-};
+opendiv.addEventListener("click", function () {
+  boxstatus.style.display = "block";
+});
+closediv.addEventListener("click", function () {
+  boxstatus.style.display = "none";
+});
